f5ba17e8ef373bd78b15e9fba9f406e96f533cad
KARTEY-JUNIOR-KWASI <karteyjunior@gmail.com>
2023-02-19 02:12:57 -0800

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,10 @@
 
 int main(void)
 {
-	char i :
-	for (i = 'a' : i <= 'z' : i++)
-			putchar(i):	
-	putchar('\n'):
-	return(0):
+	char i;
+
+	for (i = 'a' : i <= '2' : i++)
+			putchar(i) :
+	putchar('\n') :
+	return (0) :
 }
